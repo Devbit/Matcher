54019ec9d946fdc068d2fddc657f2215d72ce4d2
MrWaffle <mrwafflewaffle@aim.com>
2014-01-07 00:51:54 +0100

    Commander, multithreading, console app

--- Matcher.Console/Program.cs
+++ Matcher.Console/Program.cs
@@ -3,13 +3,127 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Matcher;
+using System.Diagnostics;
+using System.Threading;
 
-namespace Matcher.Console
+namespace Matcher.Commander
 {
     class Program
     {
+        private static string _Header = " -=Devbit Ultimate Super Matcher 2014=- ";
+        private static MatcherCommander commander;
         static void Main(string[] args)
         {
+            Console.SetWindowSize(_Header.Length, 10);
+            Console.BufferWidth = _Header.Length;
+            Console.BufferHeight = 10;
+            commander = new MatcherCommander("http://127.0.0.1:5000", false);
+            WriteMainMenu();
+        }
+
+        private static void WriteHead()
+        {
+            Console.WriteLine();
+            Console.WriteLine(_Header);
+            Console.WriteLine(" Matchers running: " + commander.GetMatcherCount());
+        }
+
+        private static int WriteMatcherList()
+        {
+            Update();
+            string status = commander.GetMatcherStatus();
+            int lines = ((status.Length - status.Replace(Environment.NewLine, "").Length) / Environment.NewLine.Length);
+            if (lines + 6 > Console.BufferHeight)
+            {
+                Console.BufferHeight = lines + 6;
+                Console.SetWindowSize(Console.BufferWidth, lines + 6);
+            }
+            Console.Write(status);
+            return lines;
+        }
+
+        private static void WriteMainMenu()
+        {
+            Update();
+            Console.WriteLine(" [1] - Start a Matcher");
+            Console.WriteLine(" [2] - Stop a Matcher");
+            Console.WriteLine(" [3] - View running Matchers");
+            Console.Write(" Enter input: ");
+            string key = Console.ReadKey().KeyChar.ToString();
+            if (!new string[3] { "1", "2", "3" }.Contains(key))
+            {
+                Update();
+                Console.WriteLine(" Invalid input, try again");
+                Console.ReadKey();
+                WriteMainMenu();
+                return;
+            }
+            switch (key)
+            {
+                case "1":
+                    WriteSpawnMenu();
+                    break;
+                case "2":
+                    WriteStopMenu();
+                    break;
+                case "3":
+                    WriteStatusMenu();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void WriteSpawnMenu()
+        {
+            commander.SpawnMatcher();
+            Update();
+            Console.WriteLine(" Matcher spawned");
+            Thread.Sleep(1000);
+            WriteMainMenu();
+        }
+
+        private static void WriteStopMenu()
+        {
+            int amount = WriteMatcherList();
+            Console.Write(" Enter input: ");
+            string input = Console.ReadKey().KeyChar.ToString();
+            int result;
+            if (int.TryParse(input, out result))
+            {
+                if (result < amount)
+                {
+                    commander.StopMatcher(result);
+                    Console.SetWindowSize(Console.BufferWidth, 10);
+                    Console.BufferHeight = 10;
+                    Update();
+                    Console.WriteLine(" Matcher stopped");
+                    Thread.Sleep(1000);
+                    WriteMainMenu();
+                    return;
+                }
+            }
+            Update();
+            Console.WriteLine(" Invalid input, try again");
+            Console.ReadKey();
+            WriteStopMenu();
+        }
+
+        private static void WriteStatusMenu()
+        {
+            WriteMatcherList();
+            Console.ReadKey();
+            Console.SetWindowSize(Console.BufferWidth, 10);
+            Console.BufferHeight = 10;
+            WriteMainMenu();
+        }
+
+        private static void Update()
+        {
+            Console.Clear();
+            WriteHead();
+            Console.WriteLine();
         }
     }
 }
--- Matcher/Matcher.cs
+++ Matcher/Matcher.cs
@@ -2,11 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Communicator;
+using System.Collections.Concurrent;
+using System.Diagnostics;
 
 namespace Matcher
 {
     public class Matcher
     {
+        private bool _isRunning = false;
+        private MatcherCommander Commander;
+        private Processor proc;
+        private string status = "";
+
+        private Matcher() { }
+
+        public Matcher(Processor processor, MatcherCommander commander)
+        {
+            proc = processor;
+            Commander = commander;
+        }
+
+        public string GetStatus()
+        {
+            return status;
+        }
+
+        public void Start(out Thread thread)
+        {
+            _isRunning = true;
+            thread = new Thread(new ThreadStart(Run));
+            thread.Start();
+        }
+
+        public void Stop()
+        {
+            _isRunning = false;
+            status = "stopping";
+        }
+
+        private void Run()
+        {
+            List<Profile> profiles;
+            List<Vacancy> vacancies;
+            status = "init";
+            Debug.WriteLine("Init");
+            while (_isRunning)
+            {
+                //profiles = proc.GetNextProfiles();
+                //vacancies = proc.GetNextVacancies();
+                //Do matching algorithm here
+                Debug.WriteLine("Start");
+                status = "matching";
+                Thread.Sleep(5000);
+                //Debug.WriteLine("Done");
+            }
+            Commander.FinishMatcher(this);
+            
+        }
     }
 }
