8dba55a12c57d5bc991987c342ca234e9ea950a7
Giovanni Paul <giovannipaul@live.nl>
2014-01-16 19:17:10 +0100

    #ennuweerwel #kutgit

---
+++ Matcher/Interfaces/IAlgorithm.cs
@@ -0,0 +1,24 @@
+﻿//           ■■■■   ■■■■■■              
+//           ■■■■   ■■■■■■       © Copyright 2014         
+//           ■■■■         ■■■     _____             _     _ _           _
+//           ■■■■   ■■■   ■■■    |  __ \           | |   (_) |         | |
+//           ■■■■   ■■■   ■■■    | |  | | _____   _| |__  _| |_   _ __ | |
+//           ■■■■         ■■■    | |  | |/ _ \ \ / / '_ \| | __| | '_ \| |     
+//           ■■■■■■■■■■■■■       | |__| |  __/\ V /| |_) | | |_ _| | | | |
+//           ■■■■■■■■■■■■■       |_____/ \___| \_/ |_.__/|_|\__(_)_| |_|_|
+//
+
+using Communicator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matcher
+{
+    interface IAlgorithm
+    {
+        MatchFactor CalculateFactor<T>(Profile profile, Vacancy vacancy, int multiplier);
+    }
+}
